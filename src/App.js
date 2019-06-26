94b3dae0b4877ad692f82efc90746802c21c8aea
keekah <kika.wingert@gmail.com>
2019-06-25 22:59:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Sponsors from './pages/Sponsors';
+import Contact from './pages/Contact';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <Router>
+        <div>
+          
+          <Route exact path="/" component={Poopy} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/sponsors" component={Sponsors} />
+          <Route path="/contact" component={Contact} />
+
+        </div>
+      </Router>
+    );
+  }
+
 }
+
+const Poopy = () => {
+  return (
+    <h1>hello</h1>
+  );
+};
+
 
 export default App;
